6020abf3e97983c012774935b58c4d22ec23d335
jackatsals <80107278+jackatsals@users.noreply.github.com>
2025-12-18 15:45:31 -0500

    Update frontend.php
    Removed the aside that was part of the original splash. Meaning the post and events

--- plugins/bb-custom-modules/modules/cdlc-splash/includes/frontend.php
+++ plugins/bb-custom-modules/modules/cdlc-splash/includes/frontend.php
@@ -44,31 +44,4 @@ $query = $module->queryPosts($settings->post_type);
             </div>
         <?php endif; ?>
     </div>
-    <div class="cdlc-splash-aside">
-        <h2>
-            <?php if (!empty($settings->content_feed_icon)) : ?>
-                <i class="cdlc-content-feed__icon dark:text-white <?php echo $settings->content_feed_icon; ?>" aria-hidden="true"></i>
-            <?php endif; ?>
-            <?php echo $settings->content_feed_title; ?>
-        </h2>
-        <div class="cdlc-splash-aside__content">
-            <?php if ($query->have_posts()) : ?>
-                <?php while ($query->have_posts()) : $query->the_post(); ?>
-                    <article>
-                        <h3><a class="a11y-link-wrap" href="<?php echo get_the_permalink(); ?>"><?php echo get_the_title(); ?></a></h3>
-                            <?php if (has_post_thumbnail()) : ?>
-                                <figure>
-                                    <?php echo the_post_thumbnail('thumbnail'); ?>
-                                </figure>
-                            <?php endif; ?>
-                    </article>
-                <?php endwhile; ?>
-                <?php if (!empty($settings->link)) : ?>
-                    <a href="<?php echo $settings->link; ?>"><?php echo $settings->link_text; ?></a>
-                <?php endif; ?>
-            <?php else : ?>
-                <?php echo __('Sorry, nothing found. Please check back soon!', 'cdlc'); ?>
-            <?php endif; wp_reset_postdata(); ?>
-        </div>
-    </div>
 </section>
